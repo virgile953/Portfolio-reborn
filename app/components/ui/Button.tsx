eb46bf27d1306b493c5f3751567bb3d8d119398a
Virgile <virgile.barbera@gmail.com>
2025-01-19 17:40:34 +0100

    Added critters for css optimization and added default prefetch on button component

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 
@@ -15,20 +16,21 @@ const Button = ({
 	href: string;
 }) => {
 	return (
-		<Link href={href} passHref legacyBehavior>
-			<a
-				className={
-					`self-start relative z-10 inline-flex gap-2.5 h-12 animate-shimmer items-center justify-center
-					rounded-md border border-dark-700 bg-[linear-gradient(110deg,#1A1A1A,45%,#262626,55%,#1A1A1A)]
-					hover:bg-[linear-gradient(110deg,#262626,45%,#404040,55%,#262626)] bg-[length:200%_100%] px-6 font-medium text-sm sm:text-base
-					text-slate-200 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ` +
-					className
-				}
-			>
-				{iconPosition === "left" && icon}
-				{children}
-				{iconPosition === "right" && icon}
-			</a>
+		<Link
+			href={href}
+			passHref
+			className={
+				`self-start relative z-10 inline-flex gap-2.5 h-12 animate-shimmer items-center justify-center
+			rounded-md border border-dark-700 bg-[linear-gradient(110deg,#1A1A1A,45%,#262626,55%,#1A1A1A)]
+			hover:bg-[linear-gradient(110deg,#262626,45%,#404040,55%,#262626)] bg-[length:200%_100%] px-6 font-medium text-sm sm:text-base
+			text-slate-200 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ` +
+				className
+			}
+			prefetch={true}
+		>
+			{iconPosition === "left" && icon}
+			{children}
+			{iconPosition === "right" && icon}
 		</Link>
 	);
 };
