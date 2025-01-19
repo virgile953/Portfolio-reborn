d43c38e63f47f92607fc87d46dbc415717ab6ab7
Virgile <virgile.barbera@gmail.com>
2025-01-19 17:28:37 +0100

    removed 3d models for test page, removed test page, and added 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,24 @@
+"use client";
+import Button from "@/app/components/ui/Button";
+import { Home } from "lucide-react";
+
+const NotFoundPage = () => {
+	return (
+		<main className="min-h-screen flex flex-col items-center justify-center text-center dark:bg-dark-100 bg-white dark:bg-grid-white/[0.04] bg-grid-black/[0.06]">
+			<h1 className="text-4xl font-semibold mb-6 mt-4">
+				The page you&apos;r looking for doesn&apos;t exist (yet) ¯\_(ツ)_/¯
+			</h1>
+			<div
+				className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-dark-100 bg-white
+				[mask-image:radial-gradient(ellipse_at_center,transparent_30%,black)]"
+			/>
+			<div>
+				<Button icon={<Home />} iconPosition="left" href="/">
+					Go back home
+				</Button>
+			</div>
+		</main>
+	);
+};
+
+export default NotFoundPage;
